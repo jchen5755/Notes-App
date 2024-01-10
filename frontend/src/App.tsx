fe76fbab9d92013a027021ae734efd825b2bc537
Jerry Chen <jerrychen5755@gmail.com>
2024-01-09 17:32:36 -0800

    Connected frontend to receive json data from backend server. And added Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-bootstrap";
+import { Note } from "./models/note";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //use sate will retrun a 2 element array with a number and a function
+    const [notes, setNotes] = useState<Note[]>([]);
+
+    useEffect(() => {
+        async function loadNotes() {
+            try {
+                const response = await fetch("/api/notes", {
+                    method: "GET",
+                });
+                const notes = await response.json();
+                setNotes(notes);
+            } catch (error) {
+                console.error(error);
+                alert(error);
+            }
+        }
+        loadNotes();
+    }, []); //the empty array will cause the useEffect to execute once on render. Without it it will execute after every new render
+
+    return <div className="App">{JSON.stringify(notes)}</div>;
 }
 
 export default App;
